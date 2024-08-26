3de917c7ed45002242db3d36a98f302cbd08ce5f
karl komeya <karlkomeya04@gmail.com>
2024-08-27 08:27:25 +0900

    chore: switch from sqlite to postgres db

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "corsheaders",
+    "blog",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -82,10 +83,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prideland',
+        'USER': 'postgres',
+        'PASSWORD': 'chickenadobo',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
