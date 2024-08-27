8e77051ca65853674683d74739661a0e91c494ed
karl komeya <karlkomeya04@gmail.com>
2024-08-27 14:12:01 +0900

    fix: resolve conflicts

--- feedbacks/api/views.py
+++ feedbacks/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from ..models import Feedback
 from .serializer import FeedbackSerializer
 
+
 class FeedbackViewSet(ModelViewSet):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
