ca0ebbda4a49bfa7ddc7218011b802f6acd11653
karl komeya <karlkomeya04@gmail.com>
2024-08-30 12:44:20 +0900

    chore: update hero

--- heros/models.py
+++ heros/models.py
@@ -6,3 +6,6 @@ class Hero(models.Model):
     img_url = models.ForeignKey(Media, on_delete= models.SET_NULL, null=True)
     site_desc = models.TextField(null=False)
     logo = models.ImageField(default="default.jpg", upload_to="image_url")
+
+    def __str__(self):
+        return f"img url: {self.img_url}"
