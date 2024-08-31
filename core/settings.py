446c6978b0c8c62c4c1a47390970c3a99092b678
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-08-31 23:47:34 +0900

    fix: update vercel.json and settings.py for deployment

--- core/settings.py
+++ core/settings.py
@@ -244,7 +244,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static-build")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
