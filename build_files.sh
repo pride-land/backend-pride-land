e0d7ba454fea6e5e0ce4597e283478a9551c5d4c
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-08-31 23:21:27 +0900

    Fix: changes for deployment

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,9 @@
-bash
    python3 -m venv env
+
    source env/bin/activate
+   
    pip install -r requirements.txt
+   
    python manage.py migrate
+   
    python manage.py collectstatic
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,4 @@ urlpatterns = [
     path('api-pl/', include('pland_auth.api.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
