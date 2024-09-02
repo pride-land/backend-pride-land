a9eb79b253017a3fb4ff7f2b11b5b71bb1031fbb
susheepls <169642127+susheepls@users.noreply.github.com>
2024-09-02 15:28:25 +0900

    Fix: adjust categories and volunteers models

--- volunteers/models.py
+++ volunteers/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 import django.utils.timezone
 
+from categories.models import Categories
+
 # Create your models here.
 class Volunteer(models.Model):
     name = models.CharField(null=False)
     email = models.EmailField(null=False)
     start_date = models.DateField()
     signup_date = models.DateTimeField(default=django.utils.timezone.now, verbose_name='sign up date')
-    category = models.IntegerField(null=True, help_text="1-Weeds, 2-Shittake Mushrooms, 3-Eggs, 4-Bee Keeping, 5-Bamboo")
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=None)
     restrictions = models.TextField(default=None, blank=True, null=True)
     is_accepted = models.BooleanField(default=False, help_text='Designates whether this volunteer is accepted. Review then accept', verbose_name='active')
 
