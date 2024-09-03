ba779843838b8459c7cb092545fce3e03ce6d780
susheepls <169642127+susheepls@users.noreply.github.com>
2024-09-03 14:41:24 +0900

    Fix: edit feedbacks model to have is accepted boolean

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -5,6 +5,7 @@ class Feedback(models.Model):
 
     name = models.CharField(max_length=200, null=False)
     comment = models.TextField(null=False)
+    is_accepted = models.BooleanField(default=False, help_text='Designates whether this feedback is displayed in index', verbose_name='active')
 
     def __str__(self):
         return f"name: {self.name}, comment: {self.comment}"
