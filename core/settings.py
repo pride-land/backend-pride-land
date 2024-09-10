ca77bf0fabd78da658394a10335387aca6eff9b0
susheepls <169642127+susheepls@users.noreply.github.com>
2024-09-10 12:44:29 +0900

    Quick fix: comment in a line

--- core/settings.py
+++ core/settings.py
@@ -209,7 +209,7 @@ DATABASES = {
     }
 }
 
-# DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
