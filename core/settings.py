b0991a24bdd12ba20bba68d9555177e4aae980ca
karl komeya <karlkomeya04@gmail.com>
2024-10-16 17:43:49 +0900

    updated .env files

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 from datetime import timedelta
-load_dotenv()
+load_dotenv(override=True)
 
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
@@ -197,19 +197,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'prideland',
-        'USER': 'root',
-        'PASSWORD': 'Kimicakes19k!#%',
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
     }
 }
 
